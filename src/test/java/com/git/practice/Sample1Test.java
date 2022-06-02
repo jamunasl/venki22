3532b5f9acfe919f51b0e3febfa7674aef8eecf8
jamunasl <sljamuna.s@gmail.com>
2022-06-02 10:25:28 +0530

    handled

--- src/test/java/com/git/practice/Sample1Test.java
+++ src/test/java/com/git/practice/Sample1Test.java
@@ -5,8 +5,12 @@ import org.testng.annotations.Test;
 public class Sample1Test {
 	@Test
 	public void create1Test()
+
 	{
                //changes by venki2
+
+	//changes done by venki1
+
 		System.out.println("hiiiif");
 	}
 
